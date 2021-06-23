60d4aa061402108e6568ebada861c5ef931ef16b
Eryk Salamoński <eryk1120@gmail.com>
2021-06-23 02:39:25 +0200

    probably need to to be rewritten

--- src/ball_generator.py
+++ src/ball_generator.py
@@ -15,17 +15,42 @@ def generate_data():
         im.save(f"../raw_data/{i}.png")
     print("done")
 
+
+def normalize_array(array, value_range=(0, 1)):
+    array -= array.min()
+    array = array / array.max() * (value_range[1] - value_range[0]) + value_range[0]
+    return array
+
+
+def random_tuple_list(number_of_points=1, border_size=50, im_size=(256, 256), ball_sep_dist=60, ):
+    coordinates = []
+    for i in range(number_of_points):
+        while True:
+            # TODO that while loop can be infinite, or close to
+            #  if given wrong arguments, definitely something to repair, but works for application
+            x_c, y_c = (random.randint(border_size, im_size[0] - border_size + 1),
+                        random.randint(border_size, im_size[1] - border_size + 1))
+            # print(f"x:{x_c}, y:{y_c}")
+
+            distances = [((i[0] - x_c) ** 2 + (i[1] - y_c) ** 2) ** 0.5 for i in
+                         coordinates]  # list comprehension for euqiledian distances between freshly generated point
+            # and existing ones
+            if not distances:
+                coordinates.append((x_c, y_c))
+                break
+            elif min(distances) > ball_sep_dist:
+                coordinates.append((x_c, y_c))
+                break
+    return coordinates
+
+
 def generate_ball():
     # output image properties
     im_size = (256, 256)  # imSize
 
-    # balls geometrical placement
-    border_size = 50 # distance between balls and edge of image
-    ball_sep_dist = 60 # minimal distance between adjacent balls in positive/negative mask
-
     # werid ass parameters for phase properties of balls
     sigma_range = (10, 30)  # Bsigm
-    same_sigma = False # same sigma for all of the balls on single image;
+    same_sigma = False  # same sigma for all of the balls on single image;
 
     def simgaGenerator():
         if same_sigma:
@@ -40,48 +65,56 @@ def generate_ball():
     # choose circle coordinates
 
     max_no_balls = 10
-    coordinates =[]
+    no_balls = random.randint(1, max_no_balls)
 
-    # picking coordinates for balls
-    for i in range( random.randint(1,max_no_balls)):
-        while True:
-            #TODO that while loop can be infinite, or close to if given wrong arguments, definitely something to repair, but works for application
-            x_c, y_c = (random.randint(border_size, im_size[0]-border_size+1),
-                                  random.randint(border_size, im_size[1]-border_size+1))
-            #print(f"x:{x_c}, y:{y_c}")
+    data = {"negative": {"coordinates": [], "phase": np.zeros(im_size, dtype=float)},
+            "positive": {"coordinates": [], "phase": np.zeros(im_size, dtype=float)},
+            "merged": {"coordinates": [], "phase": np.zeros(im_size, dtype=float)}
+            }
 
-            distances = [((i[0]-x_c)**2 +(i[1]-y_c)**2)**0.5 for i in coordinates ] #list comprehension for euqiledian distances beetween freshly generated point and existing ones
-            if not distances:
-                coordinates.append((x_c, y_c))
-                break
-            elif min(distances)<ball_sep_dist:
-                coordinates.append((x_c,y_c))
-                break
+    no_balls_positive = random.randint(0, no_balls)
 
-    print(f'given c set: {coordinates}')
-    phase_plates = [np.zeros(im_size,dtype=float) for i in range(len(coordinates))]
+    data['negative']['coordinates'] = random_tuple_list(number_of_points=no_balls - no_balls_positive)
+    data['positive']['coordinates'] = random_tuple_list(number_of_points=no_balls_positive)
+    data['merged']['coordinates'] = data['positives']['coordinates'] + data['negative']['coordinates']
 
-    im_out = np.zeros(im_size,dtype=float)
+    for coordinate in data['negative']['coordinates']:
+        sign = random.choice([-1, 1])
 
-    for plate, coor in zip(phase_plates,coordinates):
-        # todo split phase image into positive and negative
-        print(coor)
-        plate[coor]=random.choice([-1,1])
-        plate = gaussian_filter(plate,next(sigma_generator))
-        im_out += plate
-        plt.imshow(plate)
-        plt.title(coor)
-        plt.colorbar()
-        plt.show()
+        plate = np.zeros(im_size, dtype=float)
+        plate[coordinate] = sign
+        plate = gaussian_filter(plate, next(sigma_generator))
+        plate = normalize_array(plate)
 
-    plt.imshow(im_out)
+        data['negative']['phase'] -= plate
+        data['merged']['phase'] += plate
+
+    for coordinate in data['positive']['coordinates']:
+        sign = random.choice([-1, 1])
+
+        plate = np.zeros(im_size, dtype=float)
+        plate[coordinate] = sign
+        plate = gaussian_filter(plate, next(sigma_generator))
+        plate = normalize_array(plate)
+
+        data['positive']['phase'] += plate
+        data['merged']['phase'] += plate
+
+    plt.imshow(data['negative']['phase'])
+    plt.title("negative")
+    plt.colorbar()
+    plt.show()
+
+    plt.imshow(data['positive']['phase'])
+    plt.title("positive")
+    plt.colorbar()
+    plt.show()
+
+    plt.imshow(data['merged']['phase'])
     plt.title("merged")
     plt.colorbar()
     plt.show()
 
 
 if __name__ == "__main__":
-
-
-
     generate_ball()
